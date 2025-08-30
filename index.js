3ddff67042a6d27c9c01cca4c38ddc8f069d5cf3
LeeZiHao <lee.zihao@ynytechnology.com.my>
2025-08-30 16:51:06 +0800

    project initialized

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, this is Node.js project API!');
+});
+
+//Normally I just put port 3000 for every project.
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Node.js project API running on http://localhost:${PORT}`);
+});
